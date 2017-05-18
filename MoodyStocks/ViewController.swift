918fb656a92161c935b461af8d429e2eb8c83fd7
Dan Chu <dzjchu@gmail.com>
2017-05-18 16:46:16 -0700

    Sentiment Score works.

--- MoodyStocks/ViewController.swift
+++ MoodyStocks/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Natural Lanaguage Understanding
+        let username = "618ab670-193e-45ba-a9ec-66207f099279"
+        let password = "1H6GGufcnMKM"
+        let version = "2017-05-18"
+        
+        let naturalLanguageUnderstanding = NaturalLanguageUnderstanding(username: username, password: password, version: version)
+        
+        let urlToAnalyze = "http://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=UAL%20site_type%3Anews%20%20language%3Aenglish"
+        
+        let features = Features(concepts: ConceptsOptions(limit: 5), sentiment:SentimentOptions(document: true, targets: ["UAL"]))
+        let parameters = Parameters(features: features, url: urlToAnalyze)
+        
+        let failure = { (error: Error) in print(error) }
+        naturalLanguageUnderstanding.analyzeContent(withParameters: parameters, failure: failure) {
+            results in 
+            print (results)
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
