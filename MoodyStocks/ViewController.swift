f3f3ed3527955d63722dfba423eaa6a2182cd9ec
Colby Behrndt <colbehr@gmail.com>
2017-05-19 08:50:18 -0700

    Uncommented Lines

--- MoodyStocks/ViewController.swift
+++ MoodyStocks/ViewController.swift
@@ -32,14 +32,14 @@ class ViewController: UITableViewController {
         
         getNumberOfPosts(urlToRequest: "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=UAL%20site_type%3Anews%20%20language%3Aenglish", term: "UAL")
         
-//        let features = Features(concepts: ConceptsOptions(limit: 5), sentiment:SentimentOptions(document: true, targets: ["UAL"]))
-//        let parameters = Parameters(features: features, url: urlToAnalyze)
-//        
-//        let failure = { (error: Error) in print(error) }
-//        naturalLanguageUnderstanding.analyzeContent(withParameters: parameters, failure: failure) {
-//            results in 
-//            print (results)
-//        }
+        let features = Features(concepts: ConceptsOptions(limit: 5), sentiment:SentimentOptions(document: true, targets: ["UAL"]))
+        let parameters = Parameters(features: features, url: urlToAnalyze)
+        
+        let failure = { (error: Error) in print(error) }
+        naturalLanguageUnderstanding.analyzeContent(withParameters: parameters, failure: failure) {
+            results in 
+            print (results)
+        }
     
     }
     func getNumberOfPosts(urlToRequest: String, term: String){
