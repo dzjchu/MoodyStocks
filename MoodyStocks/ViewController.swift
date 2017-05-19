2b289603d8cb3b32fa3b455880d2520c87ff545f
Colby Behrndt <colbehr@gmail.com>
2017-05-18 21:51:40 -0700

    Finished GetNumberOfPosts, added term dict

--- MoodyStocks/ViewController.swift
+++ MoodyStocks/ViewController.swift
@@ -11,6 +11,7 @@ import NaturalLanguageUnderstandingV1
 import Foundation
 class ViewController: UITableViewController {
 
+    var numsDict : Dictionary<String, Int> = Dictionary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,65 +24,83 @@ class ViewController: UITableViewController {
         
         let naturalLanguageUnderstanding = NaturalLanguageUnderstanding(username: username, password: password, version: version)
         
-        let urlToAnalyze = "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=UAL%20site_type%3Anews%20%20language%3Aenglish"
-        getNumberOfPosts(urlToRequest: urlToAnalyze)
+        let urlToAnalyze = "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=apple%20site_type%3Anews%20%20language%3Aenglish"
         
+        // Send in a url and the searchterm used in the url
+        // Will add the amount of articles to the 'numsDict' using the key 'term'
+        getNumberOfPosts(urlToRequest: urlToAnalyze, term: "Apple")
         
-        let features = Features(concepts: ConceptsOptions(limit: 5), sentiment:SentimentOptions(document: true, targets: ["UAL"]))
-        let parameters = Parameters(features: features, url: urlToAnalyze)
+        getNumberOfPosts(urlToRequest: "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=UAL%20site_type%3Anews%20%20language%3Aenglish", term: "UAL")
         
-        let failure = { (error: Error) in print(error) }
-        naturalLanguageUnderstanding.analyzeContent(withParameters: parameters, failure: failure) {
-            results in 
-            print (results)
-        }
+//        let features = Features(concepts: ConceptsOptions(limit: 5), sentiment:SentimentOptions(document: true, targets: ["UAL"]))
+//        let parameters = Parameters(features: features, url: urlToAnalyze)
+//        
+//        let failure = { (error: Error) in print(error) }
+//        naturalLanguageUnderstanding.analyzeContent(withParameters: parameters, failure: failure) {
+//            results in 
+//            print (results)
+//        }
     
     }
-    
-//    // awesome
-//    func getNumberOfPosts(urlInput: String){
-//        let urlString = URL(string: urlInput)
-//        if let url = urlString {
-//            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-//                if error != nil {
-//                    print(error!)
-//                } else {
-//                    if let usableData = data {
-//                        print(usableData)
-//                        print(data)
-////                        Data().base64EncodedString()
-//                        print(usableData) //JSONSerialization
-//                    }
-//                }
-//            }
-//            task.resume()
-//    }}
-//    
-    func getNumberOfPosts(urlToRequest: String){
+    func getNumberOfPosts(urlToRequest: String, term: String){
         let url4 = URL(string: urlToRequest)!
         let session4 = URLSession.shared
         let request = NSMutableURLRequest(url: url4)
-        request.httpMethod = "POST"
+        request.httpMethod = "GET"
         request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
-        let paramString = "data=Hello"
+        let paramString = ""
         request.httpBody = paramString.data(using: String.Encoding.utf8)
         let task = session4.dataTask(with: request as URLRequest) { (data, response, error) in
             guard let _: Data = data, let _: URLResponse = response, error == nil else {
                 print("*****error")
                 return
             }
+            //There is lots of bad variable names here, sorry
+            //Starting with the full string(dataString) convert to utf8
             let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-//            print("*****This is the data 4: \(dataString)") //JSONSerialization
-            let totalResultsIndex = dataString?.range(of: "\"totalResults\": ", options: String.CompareOptions.backwards).location
-            print(totalResultsIndex)
+            //get the index of that string + 16
+            let totalResultsIndex = (dataString?.range(of: "\"totalResults\": ", options: String.CompareOptions.backwards).location)! + 16
+            //get the substring from that index to the end of the string (Chops the first ~8000 chars or whatever)
+            let newString = dataString?.substring(from: totalResultsIndex)
+            //makes a new index for the first comma in the string
+            let range: Range<String.Index> = newString!.range(of: ",")!
+            //sets up another index or something
+            let index: Int = newString!.distance(from: newString!.startIndex, to: range.lowerBound)
+            //cuts everything off from the comma to the end leaving you with a string that is the number and add to dict
+            self.numsDict[term] = Int((newString?.substring(to: index))!)
+            
+            print(self.numsDict)
         }
         task.resume()
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+}
+
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return substring(from: fromIndex)
+    }
+    
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return substring(to: toIndex)
+    }
+    
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return substring(with: startIndex..<endIndex)
+    }
 }
 
