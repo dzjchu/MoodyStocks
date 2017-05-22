59cd30ad525560a66e961f5535786454452d1108
Dan Chu <dzjchu@gmail.com>
2017-05-22 10:45:25 -0700

    Fix additional commits

---
+++ MoodyStocks/CustomCell.swift
@@ -0,0 +1,166 @@
+//
+//  CustomCell.swift
+//  MoodyStocks
+//
+//  Created by Istvan on 5/19/17.
+//  Copyright © 2017 AlphaChron. All rights reserved.
+//
+
+import UIKit
+import NaturalLanguageUnderstandingV1
+import Foundation
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var leftView: UIView!
+    @IBOutlet weak var progressBar: UIProgressView!    
+    var symbol: String = ""
+    var numsDict : Dictionary<String, Int> = Dictionary()
+    let green = UIColor(red:0.33, green:0.76, blue:0.37, alpha:1.0)
+    let red = UIColor(red:0.82, green:0.33, blue:0.33, alpha:1.0)
+    
+    func updateProgressBar(withRatio: Float){
+        print("ProgressBar --> \(withRatio)")
+        
+        DispatchQueue.main.async {
+            if withRatio < 0.25{
+                self.progressBar.setProgress(withRatio + 0.5, animated: true)
+            }else {
+                self.progressBar.setProgress(withRatio, animated: true)
+            }
+        }
+    }
+
+    //1: get number of articles
+    //2: Call Watson 
+    //3: Update progessBar
+    //4: Update mood
+    func setBgColor(Color: UIColor = UIColor(red:0.59, green:0.59, blue:0.59, alpha:1.0)){
+        self.leftView.backgroundColor = Color
+    }
+    
+    func getNumberOfNewsCounts(term: String){
+        let urlShort = "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=\(term)%20site_type%3Anews%20%20language%3Aenglish"
+        let urlNumCount = "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1492625498800&sort=crawled&q=\(term)%C2%A0%20language%3Aenglish%20site_type%3Anews"
+        getNumberOfPosts(urlToRequest: urlShort, term: term, maxCall: false)
+        getNumberOfPosts(urlToRequest: urlNumCount, term: term, maxCall: true)
+    }
+    
+    
+    //Get Count from JSON
+    
+    // Send in a url and the searchterm used in the url
+    // Will add the amount of articles to the 'numsDict' using the key 'term'
+    func getNumberOfPosts(urlToRequest: String, term: String, maxCall: Bool){
+        let url4 = URL(string: urlToRequest)!
+        let session4 = URLSession.shared
+        let request = NSMutableURLRequest(url: url4)
+        request.httpMethod = "GET"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        let paramString = ""
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        let task = session4.dataTask(with: request as URLRequest) { (data, response, error) in
+            guard let _: Data = data, let _: URLResponse = response, error == nil else {
+                print("*****error")
+                return
+            }
+            //There is lots of bad variable names here, sorry
+            //Starting with the full string(dataString) convert to utf8
+            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            //get the index of that string + 16
+            //print(dataString!)
+            if((dataString?.length)! < 50){
+                let error = true
+                print("error")
+                return 
+            }
+            let totalResultsIndex = (dataString?.range(of: "\"totalResults\": ", options: String.CompareOptions.backwards).location)! + 16
+            //get the substring from that index to the end of the string (Chops the first ~8000 chars or whatever)
+            let newString = dataString?.substring(from: totalResultsIndex)
+            //makes a new index for the first comma in the string
+            let range: Range<String.Index> = newString!.range(of: ",")!
+            //sets up another index or something
+            let index: Int = newString!.distance(from: newString!.startIndex, to: range.lowerBound)
+            //cuts everything off from the comma to the end leaving you with a string that is the number and add to dict
+            if maxCall{
+                self.numsDict[term+"Max"] = Int((newString?.substring(to: index))!)
+            }else{
+                self.numsDict[term] = Int((newString?.substring(to: index))!)
+            }
+           print(self.numsDict)
+            
+            if maxCall && error != nil{
+                DispatchQueue.main.async {
+                    self.callsOnWatson(urlInput: term)
+//                    self.updateProgressBar(withRatio: Float(self.numsDict[self.symbol]!/self.numsDict[self.symbol+"Max"]!))
+                }
+            }
+            
+            //RANDOM NUM
+            if(maxCall){
+                self.callsOnWatson(urlInput: term)
+                self.updateProgressBar(withRatio: Float(arc4random()) / Float(UINT32_MAX))
+            }
+        }
+        task.resume()
+    }
+    
+    func callsOnWatson(urlInput: String){
+        let urlShort = "https://webhose.io/filterWebContent?token=fc746d99-b259-4b82-925e-8128046b5b0e&format=json&ts=1495055830238&sort=relevancy&q=\(urlInput)%20site_type%3Anews%20%20language%3Aenglish"
+        
+        // Natural Lanaguage Understanding
+        let username = "618ab670-193e-45ba-a9ec-66207f099279"
+        let password = "1H6GGufcnMKM"
+        let version = "2017-05-18"
+        
+        let naturalLanguageUnderstanding = NaturalLanguageUnderstanding(username: username, password: password, version: version)
+        
+        let features = Features(concepts: ConceptsOptions(limit: 5), sentiment:SentimentOptions(document: true, targets: [self.symbol]))
+        let parameters = Parameters(features: features, url: urlShort)
+        
+        let failure = { (error: Error) in print(error) }
+        naturalLanguageUnderstanding.analyzeContent(withParameters: parameters, failure: failure) {
+            results in
+            
+        DispatchQueue.main.async {
+            //UPDATE ME
+            let random  = Float(arc4random()) / Float(UINT32_MAX)
+            print(random)
+            if(random > 0.5){
+                self.leftView.backgroundColor = UIColor(red:0.82, green:0.33, blue:0.33, alpha:1.0)
+                print("me")
+            } else{
+                self.leftView.backgroundColor = UIColor(red:0.33, green:0.76, blue:0.37, alpha:1.0)
+                  print("gme")
+            }
+            }
+        }
+    }
+}
+
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return substring(from: fromIndex)
+    }
+    
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return substring(to: toIndex)
+    }
+    
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return substring(with: startIndex..<endIndex)
+    }
+}
+
+
+
+
+
---
+++ MoodyStocks/ViewController.swift
@@ -0,0 +1,164 @@
+//
+//  ViewController.swift
+//  MoodyStocks
+//
+//  Created by Dan Chu on 5/18/17.
+//  Copyright © 2017 AlphaChron. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+	var myStocks: [NSManagedObject] = []
+    
+    var deleteStockIndexPath: IndexPath? = nil
+    //Loop from display
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Stock")
+        
+        do {
+            myStocks = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "New Stock",
+                                      message: "Add a new stock",
+                                      preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first,
+                let symbolToSave = textField.text else {
+                    return
+            	}
+            self.save(symbol: symbolToSave)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    func save(symbol: String) {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Stock",
+                                       in: managedContext)!
+        let newStock = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        newStock.setValue(symbol.uppercased(), forKeyPath: "symbol")
+        
+        do {
+            try managedContext.save()
+            myStocks.append(newStock)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myStocks.count
+    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        	if editingStyle == .delete {
+                deleteStockIndexPath = indexPath
+            let stockToDelete = myStocks[indexPath.row]
+            print("\(stockToDelete)")
+            confirmDelete(stock: (stockToDelete.value(forKeyPath: "symbol") as? String)!)
+        }
+    }
+
+    func handleDeleteStock(alertAction: UIAlertAction!) -> Void {
+        if let indexPath = deleteStockIndexPath {
+            tableView.beginUpdates()
+            myStocks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            deleteStockIndexPath = nil
+            tableView.endUpdates()
+        }
+    }
+    
+    func confirmDelete(stock: String) {
+        let alert = UIAlertController(title: "Delete Stock", message: "Are you sure you want to permanently delete \(stock)?", preferredStyle: .actionSheet)
+        
+        let DeleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: handleDeleteStock)
+        
+        
+        let CancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: cancelDeleteStock)
+        
+        alert.addAction(DeleteAction)
+        alert.addAction(CancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func cancelDeleteStock(alertAction: UIAlertAction!) {
+        deleteStockIndexPath = nil
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let stock = myStocks[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CustomCell
+        	cell.rightLabel?.text = stock.value(forKeyPath: "symbol") as? String
+            cell.symbol = (cell.rightLabel.text)!
+            cell.setBgColor()
+            cell.getNumberOfNewsCounts(term: cell.symbol)
+            //pull out stuff
+        return cell
+    }
+    
+}
+
+
+
+
+
+
+
+
